e37fc9f7f0e687eff7af172b28463ae4b8ed0056
arun <61176789+nakkumar@users.noreply.github.com>
2021-06-06 15:10:11 +0530

    Create mail.py

---
+++ windows/mail.py
@@ -0,0 +1,48 @@
+from selenium import webdriver
+
+import time
+
+driver = webdriver.Chrome()
+driver.get('http://192.168.1.4/webmail/src/login.php')
+
+mailid = driver.find_element_by_xpath('/html/body/form/table/tbody/tr/td/center/table/tbody/tr[2]/td/table/tbody/tr[1]/td[2]/input')
+mailid.send_keys('arunkumar.nagarajan@zippyops.in')
+
+mailidpasswd = driver.find_element_by_xpath('/html/body/form/table/tbody/tr/td/center/table/tbody/tr[2]/td/table/tbody/tr[2]/td[2]/input[1]')
+mailidpasswd.send_keys('Arun@1997')
+
+time.sleep(1)
+
+maillogin = driver.find_element_by_xpath('/html/body/form/table/tbody/tr/td/center/table/tbody/tr[3]/td/center/input')
+maillogin.click()
+
+time.sleep(2)
+
+#comp = driver.find_element_by_xpath('/html/body/table/tbody/tr[2]/td/a[1]')
+#comp.click()
+
+driver.execute_script("window.open('about:blank','secondtab');")
+driver.switch_to.window("secondtab")
+
+driver.get('http://192.168.1.4/webmail/src/compose.php?mailbox=INBOX&startMessage=1')
+
+time.sleep(1)
+
+tomail = driver.find_element_by_xpath('/html/body/form/table/tbody/tr[1]/td[2]/input')
+tomail.send_keys('admin@zippyops.in')
+
+#ccmail = driver.find_element_by_xpath('/html/body/form/table/tbody/tr[2]/td[2]/input')
+#ccmail.send_keys('DEMO-CC')
+
+#bccmail = driver.find_element_by_xpath('/html/body/form/table/tbody/tr[3]/td[2]/input')
+#bccmail.send_keys('DEMO-BCC')
+
+subject = driver.find_element_by_xpath('/html/body/form/table/tbody/tr[4]/td[2]/input')
+subject.send_keys('DEMO-SUBJECT')
+
+body = driver.find_element_by_xpath('//*[@id="body"]')
+body.send_keys('Testing')
+
+send = driver.find_element_by_xpath('/html/body/form/table/tbody/tr[8]/td/input')
+send.click()
+
